5e067b57fdbf7c518e177556792ae7ea160c13bb
jollycash <jolyonrudolph@gmail.com>
2017-03-12 11:41:29 +0100

    added comments to code

--- projecteuler/2_evenfibonacci/main.go
+++ projecteuler/2_evenfibonacci/main.go
@@ -2,27 +2,28 @@ package main
 
 import "fmt"
 
+// this function sums all even numbers in a list of integers
 func summit(z ...int) int {
-	greatest := 0
+	sumeven := 0
 	for _, v := range z {
 		if v%2 == 0 {
-			greatest = greatest + v
+			sumeven = sumeven + v
 		}
 	}
-	return greatest
+	return sumeven
 }
 
 func main() {
 	nrold := 0
 	nradd := 1
 	var nrlist []int
-	// nrlist := []int{1}
+	// the function in the range argument simulates the Fibonacci sequence
 	for i := 1; i < 40000001; func() {
 		nradd = nrold + i
 		nrold = i
 		i = nradd
 	}() {
-		nrlist = append(nrlist, nradd)
+		nrlist = append(nrlist, nradd) //alternatively could already filter for even numbers here with IF statement
 	}
 	fmt.Println("All Fibonacci numbers between 1 and 4mio:", nrlist)
 	fmt.Println("Sum of all even Fibonacci numbers between 1 and 4mio:", summit(nrlist...))
